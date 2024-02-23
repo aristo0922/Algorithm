ea7d1551b0806a2bca2409b79a63a284125f6483
aristo0922 <65939213+aristo0922@users.noreply.github.com>
2024-02-23 13:54:56 +0900

    [Silver III] Title: 주유소, Time: 40 ms, Memory: 31120 KB, Score: 41 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -4,15 +4,21 @@ cities = int(sys.stdin.readline())
 lengths = list(map(int, sys.stdin.readline().split(" ")))
 costs = list(map(int, sys.stdin.readline().split(" ")))
 
+if cities == 0 or cities > 1000:
+    exit()
+
 min_cost = costs[0]
 length = 0
 answer = 0
+
 for index in range(1, cities):
-    length += lengths[index-1]
+    length += lengths[index - 1]
     if costs[index] <= min_cost:
-        answer += length*min_cost
+        answer += length * min_cost
         min_cost = costs[index]
         length = 0
+        continue
+    if index == cities-1:
+        answer += length * min_cost
 
 print(answer)
-
