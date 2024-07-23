e3ac898e268d013ac3a877d43b989b74ba2cf733
aristo0922 <65939213+aristo0922@users.noreply.github.com>
2024-07-23 10:46:47 +0900

    [Gold III] Title: 소수의 연속합, Time: 320 ms, Memory: 38628 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -6,70 +6,48 @@ import java.util.List;
 
 public class Main {
 
-  public static int[] arr;
-  public static int sum = 0;
-  static List<Integer> selfNumArr = new ArrayList<>();
+  private static List<Integer> selfNums = new ArrayList<>();
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int target = (Integer.parseInt(br.readLine()));
-    arr = new int[target + 1];
+    int N = Integer.parseInt(br.readLine());
+    init(N);
 
-    int index = 2;
-    do {
-      selfNumArr.add(index);
-      setArr(index, target);
-      index = nextSelfNum(index+1, target);
-    } while (index <= target && index >= 0);
+    System.out.println(search(N));
+  }
 
-    int left = 0;
-    int right = 0;
-    int count = 0;
-    do {
-      if (sum < target) {
-        if(right >= selfNumArr.size()){
-          break;
+  public static void init(int N) {
+    int[] nums = new int[N + 1];
+
+    for (int i = 2; i < N + 1; i++) {
+      for (int j = i; j * i < N + 1 && i * j > 0; j++) {
+        nums[i * j]++;
+      }
+      if (nums[i] == 0) {
+        selfNums.add(i);
+      }
+    }
+  }
+
+  public static int search(int N) {
+    int l = 0, r = 0;
+    int answer = 0, sum = 0;
+    int size = selfNums.size();
+    while (l <= r && r <= size) {
+      if (sum == N) {
+        answer++;
+      }
+      if (sum <= N) {
+        if (r == size) {
+          return answer;
         }
-        sum = moveRight(sum, right);
-        right += 1;
+        sum += selfNums.get(r++);
         continue;
       }
-      if (sum > target) {
-        sum = moveLeft(sum, left);
-        left += 1;
-        continue;
+      if (sum > N) {
+        sum -= selfNums.get(l++);
       }
-      if(sum == target){
-        count += 1;
-        sum = moveLeft(sum, left);
-        left += 1;
-      }
-    } while (left < right);
-
-    System.out.println(count);
-  }
-
-  public static int moveRight(int sum, int indexR){
-    return sum + selfNumArr.get(indexR);
-  }
-
-  public static int moveLeft(int sum, int indexL){
-    return sum - selfNumArr.get(indexL);
-  }
-
-  public static int nextSelfNum(int num, int target) {
-    if(num > target){
-      return -1;
     }
-    if (arr[num] > 0) {
-      return nextSelfNum(num + 1, target);
-    }
-    return num;
-  }
-
-  public static void setArr(int num, int target) {
-    for (int i = num; i <= target/num ; i++) {
-      arr[num * i] += 1;
-    }
+    return answer;
   }
 }
