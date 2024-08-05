ad2213f98fb09cb797925858d6d1e90371e6da91
aristo0922 <65939213+aristo0922@users.noreply.github.com>
2024-08-05 16:57:29 +0900

    [Gold I] Title: 트리의 순회, Time: 2832 ms, Memory: 49072 KB -BaekjoonHub

--- 백준/Gold/2263. 트리의 순회/트리의 순회.java
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.java
@@ -1,45 +1,59 @@
-import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	public static StringTokenizer st;
-	public static StringBuilder sb = new StringBuilder();
-	public static int INF = 100_000_000;
 
-	public static ArrayList<Integer> preorder = new ArrayList<>();
-	public static int[] inorder;
-	public static int[] postorder;
-	
-	public static void main(String[] args) throws IOException{
-		int N = Integer.parseInt(br.readLine());
-		
-		inorder = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		postorder = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		
-		getPreorder(0,N-1,0,N-1);
-		for(int i = 0 ; i < N;i++) {
-			sb.append(preorder.get(i) + " ");
-		}
-		System.out.println(sb.toString());
-	}
-	
-	public static void getPreorder(int inStart, int inEnd, int postStart, int postEnd) {
-		if(inStart>inEnd || postStart > postEnd)return;
-		
-		int root = postorder[postEnd];
-		int index = 0;
-		for(int i = 0 ; i < inorder.length;i++) {
-			if(root == inorder[i])index = i;
-		}
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static BufferedWriter sb = new BufferedWriter(new OutputStreamWriter(System.out));
+  static int[] inOrder, postOrder;
+  static int  size;
 
-		preorder.add(root);
-		getPreorder(inStart, index - 1, postStart, postStart + index - inStart - 1);
-		getPreorder(index + 1, inEnd, postStart + index - inStart, postEnd - 1);
-		
-	}
-	
+  public static void main(String[] args) throws IOException {
+    size = Integer.parseInt(br.readLine());
+    set(size);
+
+    findPreOrders(0,size - 1, 0, size - 1);
+    sb.flush();
+  }
+
+  public static void findPreOrders(int inStart, int inEnd, int postStart, int postEnd)
+      throws IOException {
+    if(inStart > inEnd || postStart > postEnd || inEnd <0 || postEnd < 0 || postStart < 0) return;
+    if(inEnd>=size || postEnd >= size) return;
+    int root = postOrder[postEnd];
+
+    int index=0;
+
+    for (int i = 0; i <= inEnd; i++) {
+      if(inOrder[i] == root){
+        index= i;
+        break;
+      }
+    }
+
+    sb.write(root + " ");
+
+    int leftSize = index - inStart;
+    findPreOrders(inStart, index - 1, postStart, postStart + leftSize - 1);
+    findPreOrders(index + 1, inEnd, postStart + leftSize, postEnd - 1);
+  }
+
+  public static void set(int size) throws IOException {
+    inOrder = new int[size];
+    postOrder = new int[size];
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < size; i++) {
+      inOrder[i] = Integer.parseInt(st.nextToken());
+    }
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < size; i++) {
+      postOrder[i] = Integer.parseInt(st.nextToken());
+    }
+  }
+
 }
